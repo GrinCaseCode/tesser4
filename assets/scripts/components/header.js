083241cd6a89a2a54ceb7fd04b01f5d72e028208
nickolay.grin <nick@singularitysolutions.com>
2024-10-04 13:19:40 +0300

    Add files via upload

--- assets/scripts/components/header.js
+++ assets/scripts/components/header.js
@@ -13,6 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const catalogButton = document.querySelector('.header-bottom-navigation-catalog-button');
   const catalog = document.querySelector('.header-catalog');
 
+  let isMouseOverCatalog = false;
+let isMouseOverButton = false;
+
+catalog.addEventListener('mouseenter', () => {
+  isMouseOverCatalog = true;
+});
+
+
   const sidebarItems = catalog.querySelectorAll('.header-catalog-sidebar__item');
   const contentItems = catalog.querySelectorAll('.header-catalog-content');
   const activeSidebarItemClass = 'header-catalog-sidebar__item_active';
@@ -38,14 +46,36 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   catalogButton.addEventListener('mouseenter', () => {
+    isMouseOverButton = true;
+  });
+
+  catalogButton.addEventListener('mouseenter', () => {
     catalog.classList.toggle('header-catalog_shown', true);
     catalogButton.classList.toggle('header-bottom-navigation-catalog-button_active', true);
   });
 
-  catalog.addEventListener('mouseleave', () => {
-    catalog.classList.toggle('header-catalog_shown', false);
-    catalogButton.classList.toggle('header-bottom-navigation-catalog-button_active', false);
+  catalogButton.addEventListener('mouseleave', () => {
+    isMouseOverButton = false;
+    setTimeout(() => {
+      if (!isMouseOverCatalog && !isMouseOverButton) {
+        catalog.classList.remove('header-catalog_shown');
+        catalogButton.classList.remove('header-bottom-navigation-catalog-button_active');
+      }
+    }, 100); // задержка для проверки положения курсора
   });
+
+
+
+  catalog.addEventListener('mouseleave', () => {
+    isMouseOverCatalog = false;
+    setTimeout(() => {
+      if (!isMouseOverCatalog && !isMouseOverButton) {
+        catalog.classList.remove('header-catalog_shown');
+        catalogButton.classList.remove('header-bottom-navigation-catalog-button_active');
+      }
+    }, 100); // задержка для проверки положения курсора
+  });
+
 });
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -61,4 +91,32 @@ document.addEventListener('DOMContentLoaded', () => {
     cart.classList.toggle('header-cart_expanded', false);
     cartPopup.classList.toggle('header-cart-popup_shown', false);
   });
+});
+
+
+$(document).ready(function() {
+  $('.header-region-select').click(function() {
+    if ($(this).siblings(".header-region__dropdown").is(":hidden")) {
+      $(this).siblings(".header-region__dropdown").fadeIn(200);
+    } else {
+      $(this).siblings(".header-region__dropdown").fadeOut (200);
+    }
+  });
+
+  $('.header-region__dropdown li').click(function() {
+    var textRegion = $(this).html();
+    $(this).siblings().removeClass("active");
+    $(this).addClass("active");
+    $(this).parents(".header-region").find(".header-region-select-button__text").html(textRegion);
+    $(this).parents(".header-region").find(".header-region__dropdown").fadeOut (200);
+  });
+
+
+  $(document).mouseup(function (e) {
+    var container = $(".header-region__dropdown");
+    if (container.has(e.target).length === 0){
+      $(".header-region__dropdown").fadeOut (200);
+    }
+  });
+
 });
---
+++ assets/scripts/components/index.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+  $('.slider-billbord').slick({
+		arrows: false,
+		dots: true,
+		infinite: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+		touchThreshold: 1000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<div class="slick-prev slick-arrow"><i class="far fa-chevron-left"></i><div/>',
+		nextArrow: '<div class="slick-next slick-arrow"><i class="far fa-chevron-right"></i><div/>',
+	});
+
+  $('.tabs-main li a').click(function(event) {
+    event.preventDefault();
+    var textTab = $(this).html();
+    $(this).parent().parent().find("li").removeClass('active');
+    $(this).parent().addClass('active');
+    $(".tab-pane-main").fadeOut(0);
+    var selectTab = $(this).attr("href");
+    $(selectTab).fadeIn(200);
+    $('.tab-container-main .slider-catalog').slick('setPosition');
+  });
+
+});
